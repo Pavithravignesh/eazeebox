0a6c5be73173cc334949a4d581bffc8f16bf5615
Pavithravignesh <pavithravignesh027@gmail.com>
2025-09-15 00:27:24 +0530

    Event page: Footer and RSVP btn has added

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
 import { Stack, useLocalSearchParams } from "expo-router";
-import { Image, Text, View } from "react-native";
+import { Image, Pressable, Text, View } from "react-native";
 import events from "../assets/events.json";
 
 export default function EventPage() {
@@ -23,6 +23,12 @@ export default function EventPage() {
             <Text className="text-lg font-semibold uppercase text-amber-800">{dayjs(event.datetime).format("ddd, D MMM")} . {dayjs(event.datetime).format("hh:mm A")}</Text>
             <Text className="text-lg">{event.description}</Text>
             <Text className="text-gray-500">{event.location}</Text>
+            <View className="absolute bottom-0 left-0 right-0 flex-row justify-between border-t-2 border-gray-400 p-3 pb-10 items-center">
+                <Text className="text-xl font-semibold">Free</Text>
+                <Pressable className="bg-red-400 p-5 px-8 rounded-xl">
+                    <Text className="text-white text-lg font-bold">Join and RSVP</Text>
+                </Pressable>
+            </View>
         </View>
     );
 }
